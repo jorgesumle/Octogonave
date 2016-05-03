41eae2e5c4de0ac1fa12403cf4a0fdf79c9eb791
jorgesumle <jorge@localhost>
2016-05-04 00:26:57 +0200

    He reducido los parámetros de casi todos los elementos del juego y creado la clase puntuación.

--- src/octogonave/Sprite.java
+++ src/octogonave/Sprite.java
@@ -67,7 +67,7 @@ public abstract class Sprite {
         spriteFrame.setTranslateY(yLocation);
         
         this.spriteImages.addAll(Arrays.asList(spriteImages));
-        scene = Octogonave.getScene();
+        scene = Main.getScene();
     }
     /**
      * Este código se actualiza en cada fotograma.
